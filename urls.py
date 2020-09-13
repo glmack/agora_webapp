db4d16bb37039ce3d45b5ec54684562cbea753f0
glmack <glmack@gmail.com>
2020-09-13 12:38:43 -0700

    Remove hello_world path from urlpatterns in line 22

--- urls.py
+++ urls.py
@@ -18,6 +18,5 @@ from django.urls import path
 from django.conf.urls import include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('hello_world.urls'))
+    path('admin/', admin.site.urls)
 ]
