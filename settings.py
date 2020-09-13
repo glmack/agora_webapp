db49215bda26232148e4245ab922f58be0e41bf1
glmack <glmack@gmail.com>
2020-09-13 11:31:26 -0700

    Add bootstrap styling and base.html plus app-level html extensions

--- settings.py
+++ settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hello_world'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'agora_webapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['agora_webapp/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
